2ce51edb20e461b8494a612d4129e345064aa652
Kelvin Charles <kelvinc1291@hotmail.com>
2025-05-28 10:21:44 -0300

    feat: implement authentication checks and add sign out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,28 @@
-const DashboardPage = () => {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+
+import { SignOutButton } from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
       <h1>Dashboard Page</h1>
+
+      <h2>{session?.user.name}</h2>
+
+      <SignOutButton />
     </div>
   );
 };
